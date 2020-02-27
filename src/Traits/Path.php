d7dae15671ccc7db8f98475d55c4b0c50d66c4e7
MilesChou <jangconan@gmail.com>
2020-02-27 22:13:01 +0800

    Add with method

--- src/Traits/Path.php
+++ src/Traits/Path.php
@@ -12,6 +12,17 @@ trait Path
     private $basePath;
 
     /**
+     * @param string $path
+     * @return static
+     */
+    public function appendBasePath(string $path)
+    {
+        $this->basePath = $this->formatPath(trim($path, '/'));
+
+        return $this;
+    }
+
+    /**
      * Get base path or get location at running command
      *
      * @return string
@@ -53,13 +64,28 @@ trait Path
     }
 
     /**
-     * @param string $path
+     * Clone new instance with append base path
+     *
+     * @param string $basePath
      * @return static
      */
-    public function appendBasePath(string $path)
+    public function withAppendBasePath(string $basePath)
     {
-        $this->basePath = $this->formatPath(trim($path, '/'));
+        $clone = clone $this;
 
-        return $this;
+        return $clone->appendBasePath($basePath);
+    }
+
+    /**
+     * Clone new instance with base path
+     *
+     * @param string $basePath
+     * @return static
+     */
+    public function withBasePath(string $basePath)
+    {
+        $clone = clone $this;
+
+        return $clone->setBasePath($basePath);
     }
 }
--- tests/Codegener/Traits/PathTest.php
+++ tests/Codegener/Traits/PathTest.php
@@ -73,4 +73,19 @@ class PathTest extends TestCase
 
         $this->assertSame('/a/b/c/d/e/f', $this->target->basePath());
     }
+
+    /**
+     * @test
+     */
+    public function shouldReturnNewInstanceWhenWithMethod(): void
+    {
+        $this->target->setBasePath('/a/b/c');
+        $this->target->appendBasePath('/d/e/f');
+
+        $clone = $this->target->withBasePath('/i/j/k')
+            ->withAppendBasePath('/x/y/z');
+
+        $this->assertSame('/a/b/c/d/e/f', $this->target->basePath());
+        $this->assertSame('/i/j/k/x/y/z', $clone->basePath());
+    }
 }
